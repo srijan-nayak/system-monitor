fa856cab57d7b03c6240883a1852e6a97fab1927
Srijan Nayak <srijannayak0@gmail.com>
2022-08-20 13:23:57 +0530

    feat: Implement LinuxParser::UpTime

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -71,8 +71,18 @@ vector<int> LinuxParser::Pids() {
 // TODO: Read and return the system memory utilization
 float LinuxParser::MemoryUtilization() { return 0.0; }
 
-// TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  std::ifstream upTimeFile{LinuxParser::kProcDirectory +
+                           LinuxParser::kUptimeFilename};
+
+  string line;
+  std::getline(upTimeFile, line);
+
+  std::istringstream lineStream{line};
+  long upTime;
+  lineStream >> upTime;
+  return upTime;
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
