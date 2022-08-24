1b57c72c302d73f1f6d874455b5cc6929c66d0a0
Srijan Nayak <srijannayak0@gmail.com>
2022-08-24 20:16:32 +0530

    feat: Implement System::Processes

--- src/system.cpp
+++ src/system.cpp
@@ -25,8 +25,12 @@ format. cpp for formatting the uptime.*/
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+std::vector<Process>& System::Processes() {
+  for (const auto& pid : LinuxParser::Pids()) {
+    processes_.emplace_back(Process(pid));
+  }
+  return processes_;
+}
 
 std::string System::Kernel() { return kernel_; }
 
